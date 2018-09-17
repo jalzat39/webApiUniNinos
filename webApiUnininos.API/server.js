4bd0337e27865906e3f4b925a5245bfb5e3e2502
Mateo Marulanda <mmarulandc@eafit.edu.co>
2018-09-17 10:58:09 -0500

    usando middleware instaciado

--- webApiUnininos.API/server.js
+++ webApiUnininos.API/server.js
@@ -6,13 +6,14 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
-
-
+});
+//Middleware para servir archivos estaticos
+app.use(express.static('../webApiUniNinos-SPA/dist/webApiUniNinos-SPA'));
 //Un get solicitando '/' directory
 app.get ('/', (requests, response) => {
 
 response.send('Hello World!, from node!')
+
 
 })
 
